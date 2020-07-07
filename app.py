51c38182a0591b394b71c7bc7117b8bc402017e2
Puru Verma <puruverma0001@gmail.com>
2020-07-07 23:35:16 +0530

    Improving the code

--- app.py
+++ app.py
@@ -13,28 +13,29 @@ app = Flask(__name__)
 # Importing Automatic Music Generator
 musicGenerator = AutomaticMusicGenerator()
 
+# The entry Point
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+# Initializing the model (This will be automactically called from the templates index.html)
 @app.route('/dashboard',methods=['GET'])
 def dashboard():
     musicGenerator.setupModel();
     return render_template('dashboard.html')
 
+
+# Downloading the generated music
 @app.route('/generatemusic',methods=['GET'])
 def generateMusic():
     musicGenerator.generateMusic(musicGenerator.X_test,
                                 musicGenerator.y_test,
                                 musicGenerator.no_of_timesteps,
                                 musicGenerator.unique_x)
-    #return render_template('playmusic.html')
-    path = 'D:/Python/music.mid'
+    path = 'music.mid'
     return send_file(path, as_attachment=True)
 
-# @app.route('/download')
-# def downloadMusic():
-#    path = ''
-#    return send_file(path, as_attachment=True)
+
 
 app.run(port=5000)
